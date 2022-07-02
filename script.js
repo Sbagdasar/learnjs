1ae28c66156efe476335ae6f7d39d4363f6f47ff
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-02 13:05:53 +0300

    added comment

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-/*
+/**
+ * number exp to n
+ * @param{number} number number
+ * @param{number} n exponent
+ * @return{number} number, **n */
 function exponentN(number, n){
     return number**n;
 }
@@ -7,4 +11,4 @@ function getNumbers() {
     let numberExp = prompt("numberExp>=1", '')
     return exponentN(number, numberExp)
 }
-console.log(getNumbers())*/
+console.log(getNumbers())
