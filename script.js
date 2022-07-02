13c0dab243d9b638e46a6aa5e177fbe96f010d49
Samvel Bagdasaryan <samvelbagdasaryan1@gmail.com>
2022-07-02 11:01:18 +0300

    added html + js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log(111)
+function exponentionN(number, n){
+    return number**n;
+}
+function getNumbers() {
+   let number =  prompt("number>=1", '')
+    let numberExp = prompt("numberExp>=1", '')
+    return exponentionN(number, numberExp)
+}
+console.log(getNumbers())
